b682f59c6cd37b23ef30da9a5d4b263a2c3533b5
Sawyer Redpath <sawyerbruceredpath@gmail.com>
2018-12-12 10:14:11 -0500

    New project that is connected to luis.ai

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild BasicBot.sln -p:DeployOnBuild=true -p:PublishProfile=luis-techelevator-bot-Web-Deploy.pubxml -p:Password=pHGllBojqeP8ZFko5thhtckb5zm5BulspeDpAPvwudEpKvzkxNvtNi5aM4m2
+
