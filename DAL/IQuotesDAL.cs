02b7b860196ba311c0907a5527f1d8d8d3f98c2c
Danielle DelBrocco <dldelbrocco@gmail.com>
2018-12-14 10:22:24 -0500

    quotes DAL

---
+++ DAL/IQuotesDAL.cs
@@ -0,0 +1,13 @@
+﻿using BasicBot.Dialogs.Quotes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicBot.DAL
+{
+    public interface IQuotesDAL
+    {
+        QuoteState GetRandomQuote();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Data.SqlClient;
 using System.Text;
 
 namespace Microsoft.BotBuilderSamples
@@ -15,44 +14,6 @@ namespace Microsoft.BotBuilderSamples
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
-            try
-            {
-                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-                builder.DataSource = "studentchatbot.database.windows.net";
-                builder.UserID = "TeamStar";
-                builder.Password = "MamaLovesMe#1";
-                builder.InitialCatalog = "StudentChatBot";
-
-                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
-                {
-                    Console.WriteLine("\nQuery data example:");
-                    Console.WriteLine("=========================================\n");
-
-                    connection.Open();
-                    StringBuilder sb = new StringBuilder();
-                    sb.Append("SELECT TOP 1 quote, quote_source ");
-                    sb.Append("FROM motivational_quotes ");
-                    sb.Append("ORDER BY NEWID() ");
-                    String sql = sb.ToString();
-
-                    using (SqlCommand command = new SqlCommand(sql, connection))
-                    {
-                        using (SqlDataReader reader = command.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
-                            }
-                        }
-                    }
-                }
-            }
-            catch (SqlException e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-
-            Console.ReadLine();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
