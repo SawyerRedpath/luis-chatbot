604a0c1554145ab4c969d3387044bc3864c39e06
Danielle DelBrocco <dldelbrocco@gmail.com>
2018-12-19 12:23:54 -0500

    Updated with new endpoint key (for luis) and encrypted secret.

--- BotServices.cs
+++ BotServices.cs
@@ -29,14 +29,17 @@ namespace Microsoft.BotBuilderSamples
                 switch (service.Type)
                 {
                     case ServiceTypes.Luis:
-                        {
+                        {                            
                             var luis = (LuisService)service;
                             if (luis == null)
                             {
                                 throw new InvalidOperationException("The LUIS service is not configured correctly in your '.bot' file.");
                             }
 
+                            Console.WriteLine(luis.AuthoringKey + " - " + luis.SubscriptionKey);
+
                             var endpoint = (luis.Region?.StartsWith("https://") ?? false) ? luis.Region : luis.GetEndpoint();
+                            Console.WriteLine(endpoint);
                             var app = new LuisApplication(luis.AppId, luis.AuthoringKey, endpoint);
                             var recognizer = new LuisRecognizer(app);
                             this.LuisServices.Add(luis.Name, recognizer);
